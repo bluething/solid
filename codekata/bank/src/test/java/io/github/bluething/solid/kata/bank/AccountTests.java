743ffa5cfd88ac44cc8def976f093e12eae716c3
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-06-28 11:16:23 +0700

    Change the test class
    Add mockito extension.
    Init object for each test method

--- codekata/bank/src/test/java/io/github/bluething/solid/kata/bank/AccountTests.java
+++ codekata/bank/src/test/java/io/github/bluething/solid/kata/bank/AccountTests.java
@@ -1,14 +1,17 @@
 package io.github.bluething.solid.kata.bank;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 
 import static org.mockito.ArgumentMatchers.refEq;
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 public class AccountTests {
 
     private static final int POSITIVE_AMOUNT = 1000;
@@ -18,6 +21,11 @@ public class AccountTests {
     @Mock
     private TransactionRepository transactionRepository;
     private Account account;
+
+    @BeforeEach
+    private void setup() {
+        account = new Account(transactionRepository);
+    }
 
     @Test
     public void amountWithZeroBalanceDepositWithPositiveValueBalanceAddedWithDepositValue() {
