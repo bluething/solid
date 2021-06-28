e33943eabe03b2abaeb0084b8e23674d75e9c14c
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-06-28 10:55:31 +0700

    Add failing test

--- codekata/bank/src/test/java/io/github/bluething/solid/kata/bank/AccountTests.java
+++ codekata/bank/src/test/java/io/github/bluething/solid/kata/bank/AccountTests.java
@@ -1,14 +1,28 @@
 package io.github.bluething.solid.kata.bank;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.time.LocalDate;
+
+import static org.mockito.ArgumentMatchers.refEq;
+import static org.mockito.Mockito.verify;
 
 public class AccountTests {
 
+    private static final int POSITIVE_AMOUNT = 1000;
+    private static final int NEGATIVE_AMOUNT = -POSITIVE_AMOUNT;
+    private static final LocalDate TODAY = LocalDate.now();
+
+    @Mock
+    private Transaction transaction;
     private Account account;
 
     @Test
     public void amountWithZeroBalanceDepositWithPositiveValueBalanceAddedWithDepositValue() {
-        
+        account.deposit(POSITIVE_AMOUNT);
+        verify(transaction).add(refEq(new Transaction(TODAY, POSITIVE_AMOUNT)));
     }
 
 }
