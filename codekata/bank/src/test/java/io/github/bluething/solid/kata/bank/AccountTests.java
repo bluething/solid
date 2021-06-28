377942b9d250cafb50df687a16605a91f35a1da3
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-06-28 10:54:27 +0700

    Ignore build folder

---
+++ codekata/bank/src/test/java/io/github/bluething/solid/kata/bank/AccountTests.java
@@ -0,0 +1,14 @@
+package io.github.bluething.solid.kata.bank;
+
+import org.junit.jupiter.api.Test;
+
+public class AccountTests {
+
+    private Account account;
+
+    @Test
+    public void amountWithZeroBalanceDepositWithPositiveValueBalanceAddedWithDepositValue() {
+        
+    }
+
+}
