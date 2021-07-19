f4d319368e7f4a239f60dc804a8152f9243b04fa
bluething <habib.gunadarma@gmail.com>
2021-07-19 23:03:06 +0700

    Implement exception handling and logging at main

--- codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/SaveEmployeesMain.java
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/SaveEmployeesMain.java
@@ -1,17 +1,29 @@
 package io.github.bluething.solid.kata.globalmanticshr;
 
+import io.github.bluething.solid.kata.globalmanticshr.logging.ConsoleLogger;
 import io.github.bluething.solid.kata.globalmanticshr.persistence.Employee;
+import io.github.bluething.solid.kata.globalmanticshr.persistence.EmployeeFileSerializer;
 import io.github.bluething.solid.kata.globalmanticshr.persistence.EmployeeRepository;
 
+import java.io.IOException;
 import java.util.List;
 
 public class SaveEmployeesMain {
     public static void main(String[] args) {
-        EmployeeRepository repository = new EmployeeRepository();
+        ConsoleLogger logger = new ConsoleLogger();
+
+        EmployeeFileSerializer serializer = new EmployeeFileSerializer();
+        EmployeeRepository repository = new EmployeeRepository(serializer);
+
         List<Employee> employees = repository.findAll();
 
         for (Employee employee : employees) {
-            Employee.save(employee);
+            try {
+                repository.save(employee);
+                logger.writeInfo("Saved employee " + employee.toString());
+            } catch (IOException e) {
+                logger.writeError("Error saving employee", e);
+            }
         }
     }
 }
