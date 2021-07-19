fe059925d3e356e16f641fb32e4a4893b681091e
bluething <habib.gunadarma@gmail.com>
2021-07-19 17:01:12 +0700

    Implement save method

--- codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/persistence/Employee.java
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/persistence/Employee.java
@@ -1,5 +1,10 @@
 package io.github.bluething.solid.kata.globalmanticshr.persistence;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public abstract class Employee {
     private final String firstName;
     private final String lastName;
@@ -22,5 +27,39 @@ public abstract class Employee {
         this.monthlyIncome = monthlyIncome;
     }
 
+    public static void save(Employee employee) {
+        try {
+            StringBuilder sb = new StringBuilder();
+            sb.append("### EMPLOYEE RECORD ####");
+            sb.append(System.lineSeparator());
+            sb.append("NAME: ");
+            sb.append(employee.firstName + " " + employee.lastName);
+            sb.append(System.lineSeparator());
+            sb.append("POSITION: ");
+            sb.append(employee.getClass().getTypeName());
+            sb.append(System.lineSeparator());
+            sb.append("EMAIL: ");
+            sb.append(employee.getEmail());
+            sb.append(System.lineSeparator());
+            sb.append("MONTHLY WAGE: ");
+            sb.append(employee.monthlyIncome);
+            sb.append(System.lineSeparator());
 
+            Path path = Paths.get(employee.getFullName()
+                    .replace(" ","_") + ".rec");
+            Files.write(path, sb.toString().getBytes());
+
+            System.out.println("Saved employee " + employee.toString());
+        } catch (IOException e){
+            System.out.println("ERROR: Could not save employee. " + e);
+        }
+    }
+    public String getEmail() {
+        return this.firstName + "." +
+                this.lastName +
+                "@globomanticshr.com";
+    }
+    public String getFullName(){
+        return this.firstName + " " + this.lastName;
+    }
 }
