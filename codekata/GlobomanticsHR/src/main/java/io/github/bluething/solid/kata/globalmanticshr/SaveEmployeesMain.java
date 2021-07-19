b7c34d775a8564302453f0c038910cfeb797f584
bluething <habib.gunadarma@gmail.com>
2021-07-19 16:52:21 +0700

    Remove unused import

--- codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/SaveEmployeesMain.java
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/SaveEmployeesMain.java
@@ -2,8 +2,6 @@ package io.github.bluething.solid.kata.globalmanticshr;
 
 import io.github.bluething.solid.kata.globalmanticshr.persistence.EmployeeRepository;
 
-import java.util.List;
-
 public class SaveEmployeesMain {
     public static void main(String[] args) {
         EmployeeRepository repository = new EmployeeRepository();
