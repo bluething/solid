77eba5fb2c127edb9eb4d1325e3f288927e8f4eb
bluething <habib.gunadarma@gmail.com>
2021-07-19 16:59:09 +0700

    Employee parent constructor

--- codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/persistence/Employee.java
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/persistence/Employee.java
@@ -1,5 +1,26 @@
 package io.github.bluething.solid.kata.globalmanticshr.persistence;
 
 public abstract class Employee {
+    private final String firstName;
+    private final String lastName;
+    private int monthlyIncome;
+    private int nbHoursPerWeek;
+
+    public Employee(String fullName, int monthlyIncome) {
+        setMonthlyIncome(monthlyIncome);
+
+        String[] names = fullName.split(" ");
+        this.firstName = names[0];
+        this.lastName = names[1];
+    }
+
+    public void setMonthlyIncome(int monthlyIncome) {
+        if(monthlyIncome < 0){
+            throw new IllegalArgumentException("Income must be positive");
+        }
+
+        this.monthlyIncome = monthlyIncome;
+    }
+
 
 }
