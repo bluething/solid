6eb66ea7c250b2f85e72d011853a4a4586844071
bluething <habib.gunadarma@gmail.com>
2021-07-19 16:52:11 +0700

    Create findAll() method at repository class

--- codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/persistence/EmployeeRepository.java
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/persistence/EmployeeRepository.java
@@ -1,5 +1,16 @@
 package io.github.bluething.solid.kata.globalmanticshr.persistence;
 
+import java.util.Arrays;
+
 public class EmployeeRepository {
-    
+
+    public List<Employee> findAll() {
+        Employee anna = new FullTimeEmployee("Anna Smith", 2000);
+        Employee billy = new FullTimeEmployee("Billy Leech", 920);
+
+        Employee steve = new PartTimeEmployee("Steve Jones", 800);
+        Employee magda = new PartTimeEmployee("Magda Iovan", 920);
+
+        return Arrays.asList(anna, billy, steve, magda);
+    }
 }
