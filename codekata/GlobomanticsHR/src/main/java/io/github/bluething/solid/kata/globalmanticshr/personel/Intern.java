61fab13985d47a5d6a3881779569230806455d89
bluething <habib.gunadarma@gmail.com>
2021-07-27 13:37:30 +0700

    Add new employee type

---
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/personel/Intern.java
@@ -0,0 +1,8 @@
+package io.github.bluething.solid.kata.globalmanticshr.personel;
+
+public class Intern extends Employee {
+    public Intern(String fullName, int monthlyIncome, int nbHours) {
+        super(fullName, monthlyIncome);
+        setNbHoursPerWeek(nbHours);
+    }
+}
