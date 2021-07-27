3a6bf2486d9f3a0a92e8c99cc8e6613af2e3bf56
bluething <habib.gunadarma@gmail.com>
2021-07-27 13:30:32 +0700

    The main method

---
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/CalculateTaxMain.java
@@ -0,0 +1,39 @@
+package io.github.bluething.solid.kata.globalmanticshr;
+
+import io.github.bluething.solid.kata.globalmanticshr.logging.ConsoleLogger;
+import io.github.bluething.solid.kata.globalmanticshr.persistence.EmployeeFileSerializer;
+import io.github.bluething.solid.kata.globalmanticshr.persistence.EmployeeRepository;
+import io.github.bluething.solid.kata.globalmanticshr.personel.Employee;
+import io.github.bluething.solid.kata.globalmanticshr.taxes.TaxCalculator;
+
+import javax.swing.*;
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
+
+public class CalculateTaxMain {
+    public static void main(String[] args) {
+        ConsoleLogger logger = new ConsoleLogger();
+        EmployeeFileSerializer fileSerializer = new EmployeeFileSerializer();
+        EmployeeRepository repository = new EmployeeRepository(fileSerializer);
+
+        List<Employee> employees = repository.findAll();
+
+        Locale locale = new Locale("en", "US");
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(locale);
+        TaxCalculator taxCalculator = new TaxCalculator();
+
+        double totalTax = 0.0;
+        double tax = 0.0;
+        String formattedTax = "";
+        for (Employee employee : employees) {
+            tax = taxCalculator.calculate(employee);
+            formattedTax = currencyFormat.format(tax);
+            logger.writeInfo(employee.getFullName() + " taxes = " + formattedTax);
+
+            totalTax += tax;
+        }
+
+        logger.writeInfo("Total taxes = " + currencyFormat.format(totalTax));
+    }
+}
