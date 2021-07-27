d6ec48605f083d9a2362b39ef6e1f9399bc1a413
bluething <habib.gunadarma@gmail.com>
2021-07-27 14:03:22 +0700

    Create an interface

---
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/taxes/TaxCalculator.java
@@ -0,0 +1,7 @@
+package io.github.bluething.solid.kata.globalmanticshr.taxes;
+
+import io.github.bluething.solid.kata.globalmanticshr.personel.Employee;
+
+public interface TaxCalculator {
+    double calculate(Employee employee);
+}
