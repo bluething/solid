95ed86d0d62b6877add0732e69dd05f57127c210
bluething <habib.gunadarma@gmail.com>
2021-07-27 13:42:30 +0700

    Implement tax calculator new requirement without ocp

--- codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/taxes/TaxCalculator.java
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/taxes/TaxCalculator.java
@@ -1,6 +1,9 @@
 package io.github.bluething.solid.kata.globalmanticshr.taxes;
 
 import io.github.bluething.solid.kata.globalmanticshr.personel.Employee;
+import io.github.bluething.solid.kata.globalmanticshr.personel.FullTimeEmployee;
+import io.github.bluething.solid.kata.globalmanticshr.personel.Intern;
+import io.github.bluething.solid.kata.globalmanticshr.personel.PartTimeEmployee;
 
 public class TaxCalculator {
     private final int RETIREMENT_TAX_PERCENTAGE = 10;
@@ -8,8 +11,28 @@ public class TaxCalculator {
     private final int BASE_HEALTH_INSURANCE = 100;
 
     public double calculate(Employee employee) {
-        return BASE_HEALTH_INSURANCE
-                + (employee.getMonthlyIncome() * RETIREMENT_TAX_PERCENTAGE) / 100
-                + (employee.getMonthlyIncome() * INCOME_TAX_PERCENTAGE) / 100;
+        int monthlyIncome = employee.getMonthlyIncome();
+
+        if (employee instanceof FullTimeEmployee) {
+            return BASE_HEALTH_INSURANCE +
+                    (monthlyIncome * RETIREMENT_TAX_PERCENTAGE) / 100 +
+                    (monthlyIncome * INCOME_TAX_PERCENTAGE) / 100;
+        }
+
+        if (employee instanceof PartTimeEmployee) {
+            return BASE_HEALTH_INSURANCE +
+                    (monthlyIncome * 10) / 100 +
+                    (monthlyIncome * INCOME_TAX_PERCENTAGE) / 100;
+        }
+
+        if (employee instanceof Intern) {
+            if (monthlyIncome < 350) {
+                return 0;
+            } else {
+                return (monthlyIncome * INCOME_TAX_PERCENTAGE) / 100;
+            }
+        }
+
+        return 0;
     }
 }
