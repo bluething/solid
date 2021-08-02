03129eb5d50d4d3085ff1dfe8a4c8b8146829853
bluething <habib.gunadarma@gmail.com>
2021-08-02 11:14:31 +0700

    Main method for requesting time off
    It will throw RuntimeException for Sub contractor employee type
    Also error when calculating tax because we don't have calculator for Sub contractor employee type

---
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/NatHolidayEmployeeTimeOffMain.java
@@ -0,0 +1,24 @@
+package io.github.bluething.solid.kata.globalmanticshr;
+
+import io.github.bluething.solid.kata.globalmanticshr.logging.ConsoleLogger;
+import io.github.bluething.solid.kata.globalmanticshr.persistence.EmployeeFileSerializer;
+import io.github.bluething.solid.kata.globalmanticshr.persistence.EmployeeRepository;
+import io.github.bluething.solid.kata.globalmanticshr.personel.Employee;
+import io.github.bluething.solid.kata.globalmanticshr.personel.FullTimeEmployee;
+
+import java.util.List;
+
+public class NatHolidayEmployeeTimeOffMain {
+    public static void main(String[] args) {
+        ConsoleLogger logger = new ConsoleLogger();
+        EmployeeFileSerializer fileSerializer = new EmployeeFileSerializer();
+        EmployeeRepository repository = new EmployeeRepository(fileSerializer);
+
+        List<Employee> employees = repository.findAll();
+        Employee manager = new FullTimeEmployee("Steve Jackson", 5000);
+
+        for (Employee employee : employees) {
+            employee.requestTimeOff(1, manager);
+        }
+    }
+}
