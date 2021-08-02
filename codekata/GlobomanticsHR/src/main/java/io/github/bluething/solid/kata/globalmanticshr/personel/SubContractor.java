25f0db9259ff59286fa08f057a8010af585e429a
bluething <habib.gunadarma@gmail.com>
2021-08-02 11:22:17 +0700

    Break incorrect relationship

--- codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/personel/SubContractor.java
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/personel/SubContractor.java
@@ -1,24 +1,30 @@
 package io.github.bluething.solid.kata.globalmanticshr.personel;
 
-public class SubContractor extends Employee {
-    public SubContractor(String fullName, int monthlyIncome) {
-        super(fullName, monthlyIncome);
-    }
+public class SubContractor {
+    private final int monthlyIncome;
+    private int nbHoursPerWeek;
+    private String email;
+    private String name;
 
-    @Override
-    public void requestTimeOff(int nbDays, Employee manager) {
-        throw new RuntimeException("Not implemented");
+    public SubContractor(String name,
+                         String email,
+                         int monthlyIncome,
+                         int nbHoursPerWeek) {
+
+        this.name = name;
+        this.email = email;
+        this.monthlyIncome = monthlyIncome;
+        this.nbHoursPerWeek = nbHoursPerWeek;
     }
 
     public boolean approveSLA(ServiceLicenseAgreement sla) {
-        // Business logic for approving a service license agreement for a subcontractor
         boolean result = false;
-        if (sla.getMinUptimePercent() >= 98
-                && sla.getProblemResolutionTimeDays() <= 2) {
-            result=  true;
+        if (sla.getMinUptimePercent() >= 98 &&
+                sla.getProblemResolutionTimeDays() <= 2) {
+            result = true;
         }
 
-        System.out.println("SLA approval for " + this.getFullName() + ": " + result);
+        System.out.println("SLA approval for " + this.name + ": " + result);
         return result;
     }
 }
