8c297e9057fb797d0e606ef3bc4cc6268709b46a
bluething <habib.gunadarma@gmail.com>
2021-08-17 14:04:49 +0700

    Create 3 interfaces to replace the fat interface

---
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/documents/ExportableJson.java
@@ -0,0 +1,5 @@
+package io.github.bluething.solid.kata.globalmanticshr.documents;
+
+public interface ExportableJson {
+    String toJson();
+}
---
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/documents/ExportablePdf.java
@@ -0,0 +1,5 @@
+package io.github.bluething.solid.kata.globalmanticshr.documents;
+
+public interface ExportablePdf {
+    byte[] toPdf();
+}
