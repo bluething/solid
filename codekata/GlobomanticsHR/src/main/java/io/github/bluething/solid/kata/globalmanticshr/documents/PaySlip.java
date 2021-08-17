cc0db3eac8b24d77087a36376ed7eac39b46951d
bluething <habib.gunadarma@gmail.com>
2021-08-17 13:54:54 +0700

    Implement the abstract method
    There are two override method that we don't need now.

--- codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/documents/PaySlip.java
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/documents/PaySlip.java
@@ -1,18 +1,53 @@
 package io.github.bluething.solid.kata.globalmanticshr.documents;
 
+import io.github.bluething.solid.kata.globalmanticshr.personel.Employee;
+
+import java.time.Month;
+
 public class PaySlip implements ExportableDocument {
+
+    private final String employeeName;
+    private final int monthlyIncome;
+    private final Month month;
+
+    public PaySlip(Employee employee, Month month) {
+        this.employeeName = employee.getFullName();
+        this.monthlyIncome = employee.getMonthlyIncome();
+        this.month = month;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public int getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public Month getMonth() {
+        return month;
+    }
+
     @Override
     public byte[] toPdf() {
-        return new byte[0];
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public String toJson() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public String toTxt() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("MONTH: ").append(this.month);
+        sb.append(System.lineSeparator());
+        sb.append("NAME: ").append(this.employeeName);
+        sb.append(System.lineSeparator());
+        sb.append("INCOME: ").append(this.monthlyIncome);
+        sb.append(System.lineSeparator());
+
+        return sb.toString();
     }
 }
