7818d0a9bdb244018dab9be0bc497503a3588a87
bluething <habib.gunadarma@gmail.com>
2021-08-23 11:22:47 +0700

    Implement mail client
    Using jakarta mail library and mailtrap as a mail server

---
+++ codekata/GlobomanticsHR/src/main/java/io/github/bluething/solid/kata/globalmanticshr/notification/EmailSender.java
@@ -0,0 +1,62 @@
+package io.github.bluething.solid.kata.globalmanticshr.notification;
+
+import io.github.bluething.solid.kata.globalmanticshr.personel.Employee;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.Properties;
+
+public class EmailSender {
+    public static void notify(Employee employee) {
+        // Configure email server
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.mailtrap.io");
+        props.put("mail.smtp.port", "2525");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(
+                        "20139eacb29987",
+                        "896591c54c768e");
+            }
+        });
+
+        // Send actual email
+        try {
+            Message message = new MimeMessage(session);
+
+            // to/from
+            message.setFrom(new InternetAddress("payment@globomantics.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(employee.getEmail()));
+
+            // subject
+            message.setSubject("Salary Notification");
+
+            // content
+            String msg = "Salary sent; Value: $" + employee.getMonthlyIncome();
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setContent(msg, "text/html");
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
+            message.setContent(multipart);
+
+            // send and wait (mail server has limit)
+            Transport.send(message);
+            System.out.println("Notified " + employee.getEmail());
+            Thread.sleep(3000);
+        }
+        catch (MessagingException e){
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
